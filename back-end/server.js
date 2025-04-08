1772f4f525fbde6793c769094845f192c479cf7b
Megan Bailey <mbailey0811@yahoo.com>
2025-04-07 22:53:21 -0400

    Added quote endpoint

--- back-end/server.js
+++ back-end/server.js
@@ -39,6 +39,25 @@ app.get('/', (req, res) => {
   });
 });
 
+// Update stocks on dashboard (get quotes from Finnhub)
+app.get('/api/quote/:symbol', async (req, res) => {
+
+  // make sure symbol is uppercase
+  const symbol = req.params.symbol.toUpperCase();
+  const apiKey = config.FINNHUB_API_KEY
+
+  try {
+    // get quote data from finnhub
+    const response = await fetch(`https://finnhub.io/api/v1/quote?symbol=${symbol}&token=${apiKey}`);
+    const data = await response.json();
+    res.json(data);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Error fetching stock quote' });
+  }
+});
+
+
 // Error handling middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
